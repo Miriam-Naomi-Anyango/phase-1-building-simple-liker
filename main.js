4764e713e0e8edc834ac0df1f9ed0642df1ceed6
Miriam Anyango <minaoanya@gmail.com>
2022-06-19 18:18:50 +0300

    Done.

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+// document.addEventListener('DOMContentLoaded',() => {
+// let h = document.querySelector('#modal')
+// h.className = 'hidden'
+// })
+
 
 
 
